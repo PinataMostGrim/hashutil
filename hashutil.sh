3c5df40feb290cd2464c1738c075d76d26e7efb9
Aaron <pinatamostgrim@gmail.com>
2019-05-21 06:05:44 -0300

    Update batch file and shell script with the correct CLI script name

--- hashutil.sh
+++ hashutil.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Run hashutil.py using a virtual environment.
+# Run hashutil_cli.py using a virtual environment.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -10,10 +10,10 @@ VENV_LINUX_PATH="$SCRIPT_DIR/.venv/bin/python"
 cd "$SCRIPT_DIR"
 
 if [ -e "$VENV_WIN_PATH" ]; then
-    "$VENV_WIN_PATH" "hashutil.py" "$@"
+    "$VENV_WIN_PATH" "hashutil_cli.py" "$@"
 
 elif [ -e "$VENV_LINUX_PATH" ]; then
-    "$VENV_LINUX_PATH" "hashutil.py" "$@"
+    "$VENV_LINUX_PATH" "hashutil_cli.py" "$@"
 else
     echo "Virtual environment not found"
 fi
