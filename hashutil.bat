49e7b26b61c61588f30e90554e4dccf3ce687bfb
Aaron <pinatamostgrim@gmail.com>
2019-06-08 16:48:09 -0300

    Re-adjust batch and shell scripts to run module instead of script directly

--- hashutil.bat
+++ hashutil.bat
@@ -1,3 +1,3 @@
 @echo off
-REM: Run hashutil_cli.py using the virtual environment '.venv'
-%~dp0.venv\Scripts\python.exe %~dp0hashutil_cli.py %*
+:: Run __main__.py using the virtual environment '.venv'
+"%~dp0.venv\Scripts\python.exe" -m hashutil %*
