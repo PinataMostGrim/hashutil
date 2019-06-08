3231e68937dfdf51b752228518738108218d0be9
Aaron <pinatamostgrim@gmail.com>
2019-06-08 16:44:51 -0300

    Revert "Adjust batch and shell scripts to run module instead of script directly"
    This reverts commit b87a56205728874a03bb60e00773b0648950667a.
    
    Testing reveals that executing a script through a virtual environment's python executable directly does provide access to the virtual environment's packages. This commit is no longer necessary.

--- hashutil.bat
+++ hashutil.bat
@@ -1,3 +1,3 @@
 @echo off
-REM: Run hashutil module using the virtual environment '.venv'
-"%~dp0.venv\Scripts\activate.bat" && "%~dp0.venv\Scripts\python.exe" -m hashutil %* & "%~dp0.venv\Scripts\deactivate.bat"
+REM: Run hashutil_cli.py using the virtual environment '.venv'
+%~dp0.venv\Scripts\python.exe %~dp0hashutil_cli.py %*
